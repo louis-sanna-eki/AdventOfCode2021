9ef433d9f000d13e149d141af647e2819cbb7823
Louis <louis.sanna@ekimetrics.com>
2021-12-04 14:57:46 +0100

    Solve sample input

--- days/day4/solve.py
+++ days/day4/solve.py
@@ -2,11 +2,11 @@ file = open('days/day4/input.txt', 'r')
 lines = file.read().splitlines()
 file.close()
 
-drawnNumbers = list(map(int, lines[0].split(',')))
+numbers = list(map(int, lines[0].split(',')))
 
 boards = []
 board = []
-for index, line in enumerate(lines[1:-1]):
+for index, line in enumerate(lines[1:]):
     boardIndex = index % 6
     if (boardIndex == 0):
         board = []
@@ -16,5 +16,51 @@ for index, line in enumerate(lines[1:-1]):
         boards.append(board)
 
 
-print(drawnNumbers)
-print(boards)
+def is_board_won(board, drawnNumbers):
+    drawnNumbersDict = {
+        number: True for index, number in enumerate(drawnNumbers)
+    }
+    for row in board:
+        markedRow = True
+        for value in row:
+            if value not in drawnNumbersDict:
+                markedRow = False
+        if markedRow:
+            return True
+    for columnIndex in range(0, 5):
+        markedColumn = True
+        for rowIndex in range(0, 5):
+            value = board[rowIndex][columnIndex]
+            if value not in drawnNumbersDict:
+                markedColumn = False
+        if markedColumn:
+            return True
+    return False
+
+
+def score_board(board, drawnNumbers):
+    drawnNumbersDict = {
+        number: True for index, number in enumerate(drawnNumbers)
+    }
+    result = 0
+    for row in board:
+        for value in row:
+            if value not in drawnNumbersDict:
+                result += value
+    return result
+
+
+result = 0
+for index, number in enumerate(numbers):
+    drawnNumbers = numbers[0:index+1]
+    for board in boards:
+        if is_board_won(board, drawnNumbers):
+            boardScore = score_board(board, drawnNumbers)
+            print('boardScore', boardScore)
+            print('number', number)
+            result = boardScore * number
+            break
+    if result:
+        break
+
+print(result)
