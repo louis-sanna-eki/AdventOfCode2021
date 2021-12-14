6de54e365e9646b45f0c25509b908af789c9da1c
Louis <louis.sanna@ekimetrics.com>
2021-12-14 13:12:37 +0100

    Clean code with default dict

--- days/day14/solve-2.py
+++ days/day14/solve-2.py
@@ -1,4 +1,5 @@
 import math
+from collections import defaultdict
 
 file = open('days/day14/input.txt', 'r')
 [raw_template, raw_insertions] = file.read().split("\n\n")
@@ -20,17 +21,15 @@ NUMBER_OF_STEPS = 40
 
 first_template_letter = template[0]
 last_template_letter = template[-1]
-count_by_pair = {}
+count_by_pair = defaultdict(lambda: 0)
 
-for index, letter in enumerate(template[0:-1]):
+for index, letter in enumerate(template[:-1]):
     next_letter = template[index + 1]
     pair = letter + next_letter
-    if pair not in count_by_pair:
-        count_by_pair[pair] = 0
     count_by_pair[pair] += 1
 
 for step in range(1, NUMBER_OF_STEPS + 1):
-    new_count_by_pair = {}
+    new_count_by_pair = defaultdict(lambda: 0)
     for pair, count in count_by_pair.items():
         if pair not in insert_by_pair:
             new_count_by_pair[pair] = count
@@ -38,24 +37,16 @@ for step in range(1, NUMBER_OF_STEPS + 1):
         insert = insert_by_pair[pair]
         new_left_pair = pair[0] + insert
         new_right_pair = insert + pair[1]
-        if new_left_pair not in new_count_by_pair:
-            new_count_by_pair[new_left_pair] = 0
         new_count_by_pair[new_left_pair] += count
-        if new_right_pair not in new_count_by_pair:
-            new_count_by_pair[new_right_pair] = 0
         new_count_by_pair[new_right_pair] += count
     count_by_pair = new_count_by_pair
 
-count_by_letter = {}
+count_by_letter = defaultdict(lambda: 0)
 
 for pair, count in count_by_pair.items():
     first_letter = pair[0]
-    if first_letter not in count_by_letter:
-        count_by_letter[first_letter] = 0
     count_by_letter[first_letter] += count
     second_letter = pair[1]
-    if second_letter not in count_by_letter:
-        count_by_letter[second_letter] = 0
     count_by_letter[second_letter] += count
 
 count_by_letter[first_template_letter] += 1  # Hack to count every letter twice
