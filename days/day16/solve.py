e81690654fe0bc98ecce69b084c273cdbd867db1
Louis <louis.sanna@ekimetrics.com>
2021-12-18 00:25:40 +0100

    Parse header

--- days/day16/solve.py
+++ days/day16/solve.py
@@ -10,11 +10,20 @@ def parse_hex(char):
     return bin(int(char, BASE))[2:].zfill(NUM_OF_BITS)
 
 
+def parse_first_packet(transmission):
+    version_header = transmission[:3]
+    version = int(version_header, 2)
+    type_header = transmission[3:6]
+    type = int(type_header, 2)
+    return {"binary": transmission, "type": type, "version": version}
+
+
 def parse(transmission):
     result = []
     if (transmission == ''):
         return result
     binaries = list(map(parse_hex, transmission))
     binary_transmission = ''.join(binaries)
-    result.append({"binary": binary_transmission})
+    packet = parse_first_packet(binary_transmission)
+    result.append(packet)
     return result
--- days/day16/solve.test.py
+++ days/day16/solve.test.py
@@ -11,6 +11,14 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(parse("D2FE28")[0]
                          ["binary"], '110100101111111000101000')
 
+    def test_version(self):
+        self.assertEqual(parse("D2FE28")[0]
+                         ["version"], 6)
+
+    def test_type(self):
+        self.assertEqual(parse("D2FE28")[0]
+                         ["type"], 4)
+
 
 if __name__ == '__main__':
     unittest.main()
