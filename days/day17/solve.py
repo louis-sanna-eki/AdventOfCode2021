2b4c0447f014ec361ce7fdb9b06d1f7ee2a7170b
Louis <louis.sanna@ekimetrics.com>
2021-12-18 13:55:09 +0100

    Solve day 17 - part 1

--- days/day17/solve.py
+++ days/day17/solve.py
@@ -1,3 +1,5 @@
+import math
+
 file = open('days/day17/input.txt', 'r')
 line = file.read()
 file.close()
@@ -12,3 +14,61 @@ y_range_max = int(raw_y_range_max)
 
 print(x_range_min, x_range_max)
 print(y_range_max, y_range_max)
+
+
+def apply_step(probe):
+    # assumes initial velocity is positive
+    new_x_velocity = probe["x_velocity"] - 1 if probe["x_velocity"] > 0 else 0
+    return dict({
+        "x": probe["x"] + probe["x_velocity"],
+        "y": probe["y"] + probe["y_velocity"],
+        "x_velocity": new_x_velocity,
+        "y_velocity": probe["y_velocity"] - 1
+    })
+
+
+def is_escaped(probe):
+    x = probe["x"]
+    y = probe["y"]
+    if x > x_range_max:
+        return True
+    if y < y_range_min:
+        return True
+    return False
+
+
+def is_in_range(probe):
+    x = probe["x"]
+    y = probe["y"]
+    if x < x_range_min:
+        return False
+    if x > x_range_max:
+        return False
+    if y < y_range_min:
+        return False
+    if y > y_range_max:
+        return False
+    return True
+
+
+def compute_max_y(probe):
+    max_y = - math.inf
+    while True:
+        y = probe["y"]
+        if y > max_y:
+            max_y = y
+        if is_in_range(probe):
+            return max_y
+        if is_escaped(probe):
+            return -math.inf
+        probe = apply_step(probe)
+
+
+all_max_y = []
+for x in range(0, x_range_max):
+    for y in range(0, x_range_max):
+        probe = dict({"x": 0, "y": 0, "x_velocity": x, "y_velocity": y})
+        all_max_y.append(compute_max_y(probe))
+result = max(all_max_y)
+
+print(result)
