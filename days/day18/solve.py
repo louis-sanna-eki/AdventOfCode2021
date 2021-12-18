02aba86f8ff78c90809a16e4952516fcf166014c
Louis <louis.sanna@ekimetrics.com>
2021-12-18 19:34:21 +0100

    Make number full node to facilite navigation

--- days/day18/solve.py
+++ days/day18/solve.py
@@ -3,7 +3,7 @@ lines = file.read().splitlines()
 file.close()
 
 
-def parse_number(raw_number):
+def parse_number(raw_number, parent=None):
     if raw_number[0] == "[":
         index = 0
         unmatched_bracket_count = 0
@@ -20,18 +20,58 @@ def parse_number(raw_number):
 
         raw_left = raw_number[1:comma_index]
         raw_right = raw_number[comma_index+1:-1]
-        return dict({
+        number = dict({
+            "parent": parent,
             "left": parse_number(raw_left),
             "right": parse_number(raw_right),
+            "value": None
         })
-    return int(raw_number)
+        number["left"] = parse_number(raw_left, number)
+        number["right"] = parse_number(raw_right, number)
+        return number
+    return dict({
+        "parent": parent,
+        "left": None,
+        "right": None,
+        "value": int(raw_number)
+    })
 
-# Helper function for testing
 
-
-def number_to_string(number):
-    if isinstance(number, int):
-        return str(number)
+def number_to_string(number):  # helper function for testing
+    if number["value"] is not None:
+        return str(number["value"])
     result = "[" + number_to_string(number["left"]) + \
         "," + number_to_string(number["right"]) + "]"
     return result
+
+
+def find_with_left_dfs(number, check_condition, level=0):
+    if check_condition(number, level):
+        return number
+    found_on_left = find_with_left_dfs(
+        number["left"], check_condition, level+1)
+    if found_on_left is not None:
+        return found_on_left
+    found_on_right = find_with_left_dfs(
+        number["right"], check_condition, level+1)
+    if found_on_right is not None:
+        return found_on_right
+    return None
+
+
+def should_explode(node, level):
+    if level >= 4:
+        return True
+    return False
+
+
+def find_number_to_explode(number):
+    return find_with_left_dfs(number, should_explode)
+
+
+def isLeft(node):
+    return node["parent"]["left"] == node
+
+
+def reduce(number):
+    return number
--- days/day18/solve.test.py
+++ days/day18/solve.test.py
@@ -1,15 +1,31 @@
 import unittest
-from solve import parse_number, number_to_string
+from solve import (find_number_to_explode,
+                   number_to_string, parse_number, reduce)
 
 
 class TestDay18Methods(unittest.TestCase):
 
     def test_input_parsing(self):
-        self.assertEqual(parse_number("[[[[[9,8],1],2],3],4]")["right"], 4)
+        self.assertEqual(parse_number("[[[[[9,8],1],2],3],4]")[
+                         "right"]["value"], 4)
 
     def test_number_to_string(self):
         self.assertEqual(number_to_string(parse_number(
             "[[[[[9,8],1],2],3],4]")), "[[[[[9,8],1],2],3],4]")
+
+    def test_find_number_to_explode(self):
+        number = parse_number(
+            "[[[[[9,8],1],2],3],4]")
+        number_to_explode = find_number_to_explode(number)
+        print("number_to_explode", number_to_explode)
+        self.assertEqual(number_to_explode["left"]["value"], 9)
+        self.assertEqual(number_to_explode["right"]["value"], 8)
+
+    def test_reduce_no_regular_left(self):
+        number_to_reduce = parse_number(
+            "[[[[[9,8],1],2],3],4]")
+        number = reduce(number_to_reduce)
+        self.assertEqual(number_to_string(number), "[[[[0,9],2],3],4]")
 
 
 if __name__ == '__main__':
