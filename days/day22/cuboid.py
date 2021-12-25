5ab9d0ad6d50db82c1b082925a8c64bd9416ac67
Louis <louis.sanna@ekimetrics.com>
2021-12-25 18:18:06 +0100

    Add 1 dimensional remove

--- days/day22/cuboid.py
+++ days/day22/cuboid.py
@@ -1,14 +1,31 @@
+DIMENSIONS_COUNT = 3
+
 
 class Cuboid:
 
-    def __init__(self, coordinates: tuple):
+    def __init__(self, coordinates: list):
         self.coordinates = coordinates
 
     def intersects(self, cuboid):
         for index, (min, max) in enumerate(self.coordinates):
-            (outside_min, outside_max) = cuboid.coordinates[index]
-            if max < outside_min:
+            (to_remove_min, to_remove_max) = cuboid.coordinates[index]
+            if max < to_remove_min:
                 return False
-            if min > outside_max:
+            if min > to_remove_max:
                 return False
         return True
+
+    def remove(self, cuboid):  # return remaining space as cuboid
+        result = []
+        for index, (min, max) in enumerate(self.coordinates):
+            (to_remove_min, to_remove_max) = cuboid.coordinates[index]
+            if (min < to_remove_min):
+                new_cuboid_coordinates = self.coordinates[0:index] + [(
+                    min, to_remove_min - 1)] + self.coordinates[index+1:DIMENSIONS_COUNT]
+                result.append(Cuboid(new_cuboid_coordinates))
+            if (max > to_remove_max):
+                new_cuboid_coordinates = self.coordinates[0:index] + [(
+                    to_remove_max+1, max)] + self.coordinates[index+1:DIMENSIONS_COUNT]
+                result.append(Cuboid(new_cuboid_coordinates))
+
+        return result
--- days/day22/cuboid.test.py
+++ days/day22/cuboid.test.py
@@ -5,14 +5,44 @@ from cuboid import Cuboid
 class TestCuboid(unittest.TestCase):
 
     def test_trivial_intersect(self):
-        cuboid = Cuboid(((0, 0), (0, 0), (0, 0)))
+        cuboid = Cuboid([(0, 0), (0, 0), (0, 0)])
         result = cuboid.intersects(cuboid)
         self.assertEqual(result, True)
 
     def test_disjoint_intersect(self):
-        cuboid = Cuboid(((0, 0), (0, 0), (0, 0)))
-        result = cuboid.intersects(Cuboid(((1, 1), (0, 0), (0, 0))))
+        cuboid = Cuboid([(0, 0), (0, 0), (0, 0)])
+        result = cuboid.intersects(Cuboid([(1, 1), (0, 0), (0, 0)]))
         self.assertEqual(result, False)
+
+    def test_overlapping_intersect(self):
+        cuboid = Cuboid([(0, 2), (0, 0), (0, 0)])
+        result = cuboid.intersects(Cuboid([(1, 1), (0, 0), (0, 0)]))
+        self.assertEqual(result, True)
+
+    def test_trivial_remove(self):
+        cuboid = Cuboid([(0, 0), (0, 0), (0, 0)])
+        result = cuboid.remove(Cuboid([(0, 0), (0, 0), (0, 0)]))
+        self.assertEqual(len(result), 0)
+
+    def test_remove_top(self):
+        cuboid = Cuboid([(0, 1), (0, 0), (0, 0)])
+        result = cuboid.remove(Cuboid([(1, 1), (0, 0), (0, 0)]))
+        self.assertEqual(result[0].coordinates, [(0, 0), (0, 0), (0, 0)])
+
+    def test_remove_bottom(self):
+        cuboid = Cuboid([(0, 1), (0, 0), (0, 0)])
+        result = cuboid.remove(Cuboid([(0, 0), (0, 0), (0, 0)]))
+        self.assertEqual(result[0].coordinates, [(1, 1), (0, 0), (0, 0)])
+
+    def test_remove_y_top(self):
+        cuboid = Cuboid([(0, 0), (0, 1), (0, 0)])
+        result = cuboid.remove(Cuboid([(0, 0), (1, 1), (0, 0)]))
+        self.assertEqual(result[0].coordinates, [(0, 0), (0, 0), (0, 0)])
+
+    def test_remove_y_bottom(self):
+        cuboid = Cuboid([(0, 0), (0, 1), (0, 0)])
+        result = cuboid.remove(Cuboid([(0, 0), (0, 0), (0, 0)]))
+        self.assertEqual(result[0].coordinates, [(0, 0), (1, 1), (0, 0)])
 
 
 if __name__ == '__main__':
