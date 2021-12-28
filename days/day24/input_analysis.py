1f3ea0b48587339547be5ea032d80a4bc0d88cb3
Louis <louis.sanna@ekimetrics.com>
2021-12-28 13:18:51 +0100

    First analysis

--- days/day24/input_analysis.py
+++ days/day24/input_analysis.py
@@ -40,3 +40,47 @@ if (w == z % 26 + 12):
     z = z
 else:
     z = 26
+
+################## python translation of second sequence########################
+
+p = 1
+q = 11
+r = 12
+
+# inp w
+w = inputs[1]
+# mul x 0
+# add x z
+# mod x 26
+# add x q
+x = z % 26 + q
+# div z p
+z = z / p
+# eql x w
+# eql x 0
+x = 1 if z % 26 + q != w else 0  # x = 1 if x != w else 0
+# mul y 0
+# add y 25
+# mul y x
+# add y 1
+# mul z y
+# mul y 0
+# add y w
+# add y r
+# mul y x
+# add z y
+
+if z % 26 + q != w:  # x = 1 # z = z / p
+    # mul y 0
+    # add y 25
+    # mul y x
+    # add y 1
+    # mul z y
+    z = 26 * (z/p)
+    # mul y 0
+    # add y w
+    # add y r
+    # mul y x
+    y = w + r
+    # add z y
+    z = 26 * (z/p) + w + r
