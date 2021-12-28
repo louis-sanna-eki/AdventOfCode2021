527b094d7462c47e219fc856d6c938251421d923
Louis <louis.sanna@ekimetrics.com>
2021-12-28 15:09:49 +0100

    Parse day 29 input

--- days/day19/solve.py
+++ days/day19/solve.py
@@ -1,3 +1,15 @@
 file = open('days/day19/input.txt', 'r')
 lines = file.read().splitlines()
 file.close()
+
+scanners = list()
+scanner = list()
+
+for line in lines:
+    if line.startswith('--- scanner'):
+        scanner = list()
+        continue
+    if line == "":
+        scanners.append(scanner)
+        continue
+    scanner.append([int(coordinate) for coordinate in line.split(",")])
